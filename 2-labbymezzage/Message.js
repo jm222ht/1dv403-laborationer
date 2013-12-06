eeb1ef7f94d04de8b99d0bedad5720910d3016f3
jm222ht <jm222ht@users.noreply.github.com>
2013-12-06 12:53:05 +0100

    Update Message.js

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -1,1 +1,34 @@
-function Message(){}
+function Message (message, date){
+this.getText = function(){
+return message;
+}
+
+this.setText = function(_text){
+message = text;
+}
+
+this.getDate = function(){
+return date;
+}
+
+this.setDate = function(_date){
+date = Date;
+}
+}
+
+Message.prototype.toString = function(){
+return this.getText() + " ("+this.getDate() + ")";
+}
+
+Message.prototype.getHTMLText = function(){
+this.getHTMLText = function(){
+return this.getHTMLText();
+}
+Message.prototype.getDateText = function(){
+this.getDateText = function(){
+return this.getDateText;
+}
+}
+
+
+}
