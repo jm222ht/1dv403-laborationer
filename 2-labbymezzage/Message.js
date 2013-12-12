ce08a9c4ac2ea1370d9f6e835fdc361cf66715e1
jm222ht <jm222ht@student.lnu.se>
2013-12-12 20:47:36 +0100

    moment 5 är klart

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -1,34 +1,28 @@
-function Message (message, date){
-this.getText = function(){
-return message;
-}
-
-this.setText = function(_text){
-message = text;
-}
-
-this.getDate = function(){
-return date;
-}
-
-this.setDate = function(_date){
-date = Date;
-}
-}
-
-Message.prototype.toString = function(){
-return this.getText() + " ("+this.getDate() + ")";
-}
-
-Message.prototype.getHTMLText = function(){
-this.getHTMLText = function(){
-return this.getHTMLText();
-}
-Message.prototype.getDateText = function(){
-this.getDateText = function(){
-return this.getDateText;
-}
-}
+﻿"use strict"
 
 
+function Message(message, date) {
+
+    this.getText = function(){
+        return message;
+    }
+
+    this.setText = function (_text) {
+        message = _text;
+    }
+    this.getDate = function () {
+        return date;
+    }
+    this.setDate = function (_date) {
+        date = myDate;
+    }
 }
+
+Message.prototype.toString = function () {
+    return this.getText() + " ( " + this.getDate() + ")";
+}
+
+Message.prototype.getHTMLText() = function () {
+    return this.message.replace(/[\n\r]/g, "<br />");
+}
+
---
+++ 2-labbymezzage/myObject.js
@@ -0,0 +1,21 @@
+﻿"use strict"
+
+var myObject = {
+
+    messages:[],
+
+    init: function () {
+        
+        var link = document.getElementById("skriv");
+        link.addEventListener("click", function () {
+            var myText = (document.getElementById("text").value);
+            alert(myText);
+            var mess = new Message(myText, new Date());
+            myObject.messages.push(mess);
+            alert(myObject.messages[0]);
+        }, false);
+    }
+}
+
+window.onload = myObject.init;
+
